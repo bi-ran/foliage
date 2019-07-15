9ca8bb0faca646cea4eb3f4d0422f83b6a2cb817
Ran Bi <biran@mit.edu>
2019-07-15 14:16:05 -0400

    event: add event variables

---
+++ include/event.h
@@ -0,0 +1,33 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include "foliage.h"
+
+#include "TTree.h"
+
+#include <vector>
+
+#define B_VAL_EVT_RECO(ACTION, ...)                                         \
+    ACTION(int32_t,         hiBin,                      ## __VA_ARGS__)     \
+
+#define B_VAL_EVT_GEN(ACTION, ...)                                          \
+    ACTION(float,           Ncoll,                      ## __VA_ARGS__)     \
+
+class event {
+  public:
+    event(TTree* t, bool gen);
+
+    event(event const&) = delete;
+    event& operator=(event const&) = delete;
+    ~event() = default;
+
+    B_VAL_EVT_RECO(DECLVAL)
+    B_VAL_EVT_GEN(DECLVAL)
+
+  private:
+    void read(TTree* t);
+
+    bool _gen;
+};
+
+#endif  /* EVENT_H */
